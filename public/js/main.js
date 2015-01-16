36a30ffbf1cb8d5f6b0a8ed9a31ae2baf2153606
David <dub@zenodub.com>
2015-01-16 12:41:05 -0700

    tried viola.jones

--- public/js/main.js
+++ public/js/main.js
@@ -9,103 +9,106 @@
   var currentPlay = 0;
 
 
-  var canvas = document.getElementById("canvas1")
-  var ctx = canvas.getContext('2d');
+//   var canvas = document.getElementById("canvas1")
+//   var ctx = canvas.getContext('2d');
 
-  var canvas2 = document.getElementById("canvas2")
-  var ctx2 = canvas2.getContext('2d');
+//   var canvas2 = document.getElementById("canvas2")
+//   var ctx2 = canvas2.getContext('2d');
 
-  var canvas3 = document.getElementById("canvas3")
-  var ctx3 = canvas3.getContext('2d');
-
-
-  var localMediaStream = null;
-
-var videoElement = document.querySelector('video');
-var audioSelect = document.querySelector('select#audioSource');
-var videoSelect = document.querySelector('select#videoSource');
-var image = document.getElementById('image');
-
-navigator.getUserMedia = navigator.getUserMedia ||
-  navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-function gotSources(sourceInfos) {
-  for (var i = 0; i !== sourceInfos.length; ++i) {
-    var sourceInfo = sourceInfos[i];
-    var option = document.createElement('option');
-    option.value = sourceInfo.id;
-    if (sourceInfo.kind === 'audio') {
-      option.text = sourceInfo.label || 'microphone ' + (audioSelect.length + 1);
-      audioSelect.appendChild(option);
-    } else if (sourceInfo.kind === 'video') {
-      option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
-      videoSelect.appendChild(option);
-    } else {
-      console.log('Some other kind of source: ', sourceInfo);
-    }
-  }
-}
-
-  function snapshot() {
-    if (localMediaStream) {
-      var sw = videoElement.offsetWidth, // source width
-      sh = videoElement.offsetHeight, // source height
-      dw = canvas.width, // destination width
-      dh = canvas.height; // destination height
-      ctx.drawImage(videoElement, 0, 0, dw, dh);
-      // "image/webp" works in Chrome.
-      // Other browsers will fall back to image/png.
-      image.src = canvas.toDataURL('image/png');
-      //doFindFeatures();
-    }
-  }
+//   var canvas3 = document.getElementById("canvas3")
+//   var ctx3 = canvas3.getContext('2d');
 
 
-if (typeof MediaStreamTrack.getSources === 'undefined'){
-  alert('This browser does not support MediaStreamTrack.\n\nTry Chrome Canary.');
-} else {
-  MediaStreamTrack.getSources(gotSources);
-}
+//   var localMediaStream = null;
+
+// var videoElement = document.querySelector('video');
+// var audioSelect = document.querySelector('select#audioSource');
+// var videoSelect = document.querySelector('select#videoSource');
+// var image = document.getElementById('image');
+
+// navigator.getUserMedia = navigator.getUserMedia ||
+//   navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+// function gotSources(sourceInfos) {
+//   for (var i = 0; i !== sourceInfos.length; ++i) {
+//     var sourceInfo = sourceInfos[i];
+//     var option = document.createElement('option');
+//     option.value = sourceInfo.id;
+//     if (sourceInfo.kind === 'audio') {
+//       option.text = sourceInfo.label || 'microphone ' + (audioSelect.length + 1);
+//       audioSelect.appendChild(option);
+//     } else if (sourceInfo.kind === 'video') {
+//       option.text = sourceInfo.label || 'camera ' + (videoSelect.length + 1);
+//       videoSelect.appendChild(option);
+//     } else {
+//       console.log('Some other kind of source: ', sourceInfo);
+//     }
+//   }
+// }
+
+//   function snapshot() {
+//     if (localMediaStream) {
+//       var sw = videoElement.offsetWidth, // source width
+//       sh = videoElement.offsetHeight, // source height
+//       dw = canvas.width, // destination width
+//       dh = canvas.height; // destination height
+//       ctx.drawImage(videoElement, 0, 0, dw, dh);
+//       // "image/webp" works in Chrome.
+//       // Other browsers will fall back to image/png.
+//       image.src = canvas.toDataURL('image/png');
+//       doFindFeatures();
+//     }
+//   }
 
 
-function successCallback(stream) {
-  window.stream = stream; // make stream available to console
-  videoElement.src = window.URL.createObjectURL(stream);
-  videoElement.play();
-  localMediaStream = stream;
-}
+// if (typeof MediaStreamTrack.getSources === 'undefined'){
+//   alert
 
-function errorCallback(error){
-  console.log('navigator.getUserMedia error: ', error);
-}
 
-function start(){
-  if (!!window.stream) {
-    videoElement.src = null;
-    window.stream.stop();
-  }
-  var audioSource = audioSelect.value;
-  var videoSource = videoSelect.value;
-  var constraints = {
-    audio: {
-      optional: [{sourceId: audioSource}]
-    },
-    video: {
-      optional: [{
-       sourceId: videoSource,
-    }], 
+//   ;
+// } else {
+//   MediaStreamTrack.getSources(gotSources);
+// }
+
+
+// function successCallback(stream) {
+//   window.stream = stream; // make stream available to console
+//   videoElement.src = window.URL.createObjectURL(stream);
+//   videoElement.play();
+//   localMediaStream = stream;
+// }
+
+// function errorCallback(error){
+//   console.log('navigator.getUserMedia error: ', error);
+// }
+
+// function start(){
+//   if (!!window.stream) {
+//     videoElement.src = null;
+//     window.stream.stop();
+//   }
+//   var audioSource = audioSelect.value;
+//   var videoSource = videoSelect.value;
+//   var constraints = {
+//     audio: {
+//       optional: [{sourceId: audioSource}]
+//     },
+//     video: {
+//       optional: [{
+//        sourceId: videoSource,
+//     }], 
 
     
-    }
-  };
-  navigator.getUserMedia(constraints, successCallback, errorCallback);
+//     }
+//   };
+//   navigator.getUserMedia(constraints, successCallback, errorCallback);
 
-}
+// }
 
-audioSelect.onchange = start;
-videoSelect.onchange = start;
+// audioSelect.onchange = start;
+// videoSelect.onchange = start;
 
-start();
+// start();
 
 
 
@@ -160,7 +163,10 @@ function calculateWinLose(){
     else if ((their == "Rock") && (mine == "Scissors")) { lose ++ ; $(this).addClass('win') }
     else if ((their == "Paper") && (mine == "Rock")) { lose ++ ;$(this).addClass('win') }
     else if ((their == "Scissors") && (mine == "Paper")) { lose ++ ; $(this).addClass('win') }
-    else{ alert("?");}
+    else{
+
+
+    }
   })
   renderWinLose();
 }
@@ -175,7 +181,7 @@ function checkKey(e) {
     if (e.keyCode == '38') {
         // up arrow
         if (!activeKey){
-          setTimeout(snapshot, 200);
+          //setTimeout(snapshot, 200);
           activeKey=true;
 
           currentPlay ++;
@@ -186,7 +192,7 @@ function checkKey(e) {
           }
           else{
 
-            var thisPlay = pacBrain.myMove("normal");
+            var thisPlay = pacBrain.myMove("chaos");
             var snd = new Audio("/mp3/"+currentPlay+".mp3"); // buffers automatically when created
             snd.play();
             console.log(thisPlay);
@@ -239,212 +245,24 @@ function checkKey(e) {
 
       var doFindFeatures = function() {
         var width = 300;
-        var height = 150;
+        var height = 150
         tracking.Fast.THRESHOLD = 7;
         ctx.drawImage(image, 0, 0, width, height);
         var imageData = ctx.getImageData(0, 0, width, height);
         
 
-        var gray = tracking.Image.grayscale(imageData.data, width, height);
-        var conv = tracking.Image.sobel(gray, width, height, []);
-        
-        
-        var corners = tracking.Fast.findCorners(gray, width, height);
-        
+        console.log('deeetect!')
+        var fist = tracking.ViolaJones.detect(imageData.data, width, height, .2, 1, .05, 0.2, fistData);
 
-        var rowLastCorners = []
-        for (var i = 0; i < corners.length; i += 2) {
-          ctx.fillStyle = '#0f0';
-          ctx.fillRect(corners[i], corners[i + 1], 2, 2);
-
-          if (!rowLastCorners[corners[i+1]]) {
-            rowLastCorners[corners[i+1]] = 999;
-          }
-          if(corners[i] < rowLastCorners[corners[i+1]]){
-            rowLastCorners[corners[i+1]] = corners[i];
-          }
-        }
-        //console.log(rowLastCorners);
-       
-
-          ctx2.fillStyle = '#f00';
-          ctx2.fillRect(0,0,width,height);
-
-
-        for (var i =0; i<rowLastCorners.length; i++){
-          ctx.fillStyle = '#f00';
-          ctx.fillRect(rowLastCorners[i], i, width-rowLastCorners[i], 1);
-
-          ctx2.fillStyle = '#ff0';
-          ctx2.fillRect(rowLastCorners[i], i, width-rowLastCorners[i], 2);
-        }
-        // Pixelate
-
-
-        imageData = ctx2.getImageData(0,0, width, height);
-
-        var size = 0.5;
-
-        ctx3.mozImageSmoothingEnabled = false;
-        ctx3.webkitImageSmoothingEnabled = false;
-        ctx3.imageSmoothingEnabled = false;
-
-        /// cache scaled width and height
-        var w = Math.round(canvas2.width * size)
-        var h = Math.round(canvas2.height * size)
-
-        ctx3.drawImage(canvas2, 0, 0, w, h);
-
-
-
-        // Let's look at what we have in this image.
-        
-
-        
-        var yellowPartHeight = 0;
-        imageData = ctx3.getImageData(0,0,w,h);
+    //    var gray = tracking.Image.grayscale(imageData.data, width, height);
+    //  var conv = tracking.Image.sobel(gray, width, height, []);
         
 
 
-        var haveWeStarted = false;
-        var startPosition = 0;
-        var thisLineHeight = 0;
+        console.log('fist')
+        console.log(fist);  
 
-        var longThickLineSettings = {
-
-        }
-
-
-        var pixels=[]
-
-        var index=0;
-        var x,y
-
-        // we want to detect lines that are higher than 4px.  A line is defined as something that doesn't deviate more than 10px;
-        var pixelLineLengths = []
-        var config = {
-          acceptableLineDeviance : 20,
-          
-
-          mediumLineHeight : 3,
-          bigLineHeight : 6,
-          superBigLineHieght : 10
-
-        }
-
-        var allSmallLines = []
-        var allMediumLines = []
-        var allBigLines = []
-
-        var result = {
-
-          averageLineLength : 0,
-          averageLineDeviation : 0,
-          longestLineLength : 0,
-          shortestLineLength : 0,
-          longestMediumLine : 0,
-          quantitySmallLines: 0,
-          quantityMediumLines : 0,
-          quantityBigLines : 0,
-
-
-        }
-
-        var inLine = false;
-        var inMediumLine = false;
-        var inBigLine = false
-
-        var blockCount = 0;
-        var blockHeight = 0;
-
-         for (y = 0; y < h; y++){
-           var thisPixelLineLength = 0;
-           var yellowEnd = w ;
-
-
-
-           for (x = 0;x < w; x++){
-              var thisPixel = getPixelXY(imageData,x,y);
-              if(
-                (yellowEnd == w) && 
-                ((thisPixel[0] == 255) && (thisPixel[1] == 255))
-                ){ 
-                
-                  yellowEnd = x;
-                }
-
-
-           // console.log(x+"x"+y);
-            //console.log(getPixelXY(imageData,x,y))
-
-          } // end for y
-          //console.log(y+" _ "+yellowEnd)
-          var thisLineLength = w- yellowEnd ;
-
-          if (thisLineLength > 0 ){
-            blockCount ++;
-          }
-          else {
-            if (blockCount > blockHeight ) { blockHeight = blockCount;}
-            blockCount = 0;
-          }
-
-
-          allSmallLines.push(thisLineLength);
-
-          //console.log(allSmallLines.length + " - " + allSmallLines[allSmallLines.length-1])
-
-          // measure small lines
-          if(thisLineLength > allSmallLines[allSmallLines.length - 2]){
-             result.quantitySmallLines++;
-             //console.log('plusplus')
-          }
-
-
-          //measure medium lines
-        
-
-
-        }  // end for x
-
-        result.blockHeight = blockHeight;
-
-
-
-
-        console.log(allSmallLines)
-        var d =  allSmallLines.reduceArray();
-         result.quantityMediumLines = d.reduced;
-         result.heights = d.heights
-
-
-
-
-         if(d.reduced.length > 4) {
-           $("#guess").html("Scissors");
-
-         }
-         else {
-          if(config.blockHeight > 30){
-            $("#guess").html("Rock");
-          }
-          else {
-            $("#guess").html("Paper");
-          }
-         }
-
-
-
-         result.averageSmallLineLength = allSmallLines.avg();
-
-        
-
-        $("#debug_stuff").html("\
-          <div>acceptableLineDeviance = "+ config.acceptableLineDeviance+"</div>\
-          <div>Process canvas = "+ w +"x"+ h + "\
-          <div>Result</br>"+JSON.stringify(result).replace(/,/gi,",<br>&nbsp;&nbsp;")+"</div> \
-          <div>Config</br>"+JSON.stringify(config).replace(/,/gi,",<br>&nbsp;&nbsp;")+"</div> \
-                    ");
+        $("#debug_stuff").html("");
 
         //ctx2.drawImage(canvas2, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
         
